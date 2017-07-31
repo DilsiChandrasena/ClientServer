d6c7210af677ab50c9b4d51ed7b2926b3fc3f3eb
Dilsi <dilsimchandrasena.gmail.com>
2017-07-31 10:31:28 +0530

    Updated

--- README.txt
+++ README.txt
@@ -1,12 +1,11 @@
 DESCRIPTION
 -----------
-Within this source files,
-	Include a thread pool with minimum size of 5 and maximum size of 10.
-	Can give a number for the requests to process.
+Thread pool size and no of requests are integers and should give as terminal inputs
+	
 
 INSTRUCTION TO RUN
 ------------------
 Can run and show the output using following commands.
 	sh source.sh  pool_size  no_of_requests 
 	./source.sh  pool_size  no_of_requests 
-		(pool_size and no_of_requests are integers and should give as terminal inputs)
+		
--- ThreadPool.java
+++ ThreadPool.java
@@ -10,8 +10,8 @@ public class ThreadPool {
     public static void main(String[] args) {
          int pool_size=Integer.parseInt(args[0]);
         ExecutorService executor = Executors.newFixedThreadPool(pool_size);
-        if (5<=pool_size && pool_size<=10){
         int n=Integer.parseInt(args[1]);
+	
         for (int i = 1; i < n+1; i++) {
             Runnable worker = new WorkerThread("" + i);
             executor.execute(worker);
@@ -20,9 +20,9 @@ public class ThreadPool {
         while (!executor.isTerminated()) {
         }
         System.out.println("All the threads are finished");
-    }
-            else{
-            System.out.println("Thread pool size is mismatched");
+    
+            if (pool_size>=n){
+            System.out.println("No threads to queue");
 }
     }
 
